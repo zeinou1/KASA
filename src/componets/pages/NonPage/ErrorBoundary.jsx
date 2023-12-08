8b74c5b75ac3b5fc9588fb26d1c8119a653b430e
abdoulkader <zarikader@gmail.com>
2023-12-08 14:51:45 +0100

    Gestion erreur

---
+++ src/componets/pages/NonPage/ErrorBoundary.jsx
@@ -0,0 +1,33 @@
+import NotFound from "./NotFound.jsx";
+import React from "react";
+
+class ErrorBoundaryCustom extends React.Component {
+	constructor (props) {
+		super(props);
+		this.state = {hasError: false};
+	}
+	
+	static getDerivedStateFromError (error) {
+		console.log('getDerived')
+		return {hasError: true};
+	}
+	
+	componentDidCatch (error, errorInfo) {
+		console.log("---ERROR BOUNDARY---");
+		console.log("error : " + error);
+	}
+	
+	render () {
+		if(this.state.hasError) {
+			
+			return (
+					// retour page 404 en cas d'erreur
+					<NotFound/>
+			);
+		}
+		
+		return this.props.children;
+	}
+}
+
+export default ErrorBoundaryCustom;
